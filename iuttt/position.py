8778c686baa0a28d9207589ffe50959b0f5638c7
jw <choi.jae100@gmail.com>
2025-08-29 10:43:59 +0900

    add basic ttt game logic

--- iuttt/position.py
+++ iuttt/position.py
@@ -1,8 +1,33 @@
-
+DIM = 3
+SIZE = DIM * DIM
 
 
 class Position:
-    def __init__(self) -> None:
-        self.board = list(" " * 9)
-        self.turn = "x"
+    def __init__(self, board=" " * 9, turn="x") -> None:
+        self.board = list(board)
+        self.turn = turn
 
+    def __repr__(self):
+        return "(%s, %s)" % (repr("".join(self.board)), repr(self.turn))
+
+    def __eq__(self, other):
+        return self.board == other.board and self.turn == other.turn
+
+    def choose(self, x, o):
+        return x if self.turn == "x" else o
+
+    def move(self, index):
+        self.board[index] = self.turn
+        self.turn = self.choose("o", "x")
+        return self
+
+    def possible_moves(self):
+        return [index for index, piece in enumerate(self.board) if piece == " "]
+
+    def is_win_for(self, piece):
+        is_match = lambda line: line.count(piece) == DIM
+        rows = [is_match(self.board[i : i + DIM]) for i in range(0, SIZE, DIM)]
+        cols = [is_match(self.board[i:SIZE:DIM]) for i in range(0, DIM)]
+        maj_diag = is_match(self.board[0 : SIZE : DIM + 1])
+        min_diag = is_match(self.board[DIM - 1 : SIZE - 1 : DIM - 1])
+        return any(rows) or any(cols) or maj_diag or min_diag
--- tests/test_position.py
+++ tests/test_position.py
@@ -6,3 +6,34 @@ class TestPosisition(TestCase):
         position = Position()
         self.assertEqual(position.board, list(" " * 9))
         self.assertEqual(position.turn, "x")
+
+    def test_init_with_args(self):
+        position = Position("x        ", "o")
+        self.assertEqual(position.board, list("x        "))
+        self.assertEqual(position.turn, "o")
+
+    def test_eq(self):
+        self.assertEqual(Position(), Position())
+
+    def test_move(self):
+        self.assertEqual(Position().move(1), Position(" x       ", "o"))
+
+    def test_possible_moves(self):
+        self.assertEqual(Position().move(1).possible_moves(), [0,2,3,4,5,6,7,8])
+
+
+class TestIsWinFor(TestCase):
+    def test_no_win(self):
+        self.assertFalse(Position().is_win_for("x"))
+
+    def test_row(self):
+        self.assertTrue(Position("xxx      ").is_win_for("x"))
+
+    def test_col(self):
+        self.assertTrue(Position("o  o  o  ").is_win_for("o"))
+
+    def test_major_diagonal(self):
+        self.assertTrue(Position("x   x   x").is_win_for("x"))
+
+    def test_minor_diagonal(self):
+        self.assertTrue(Position("  x x x  ").is_win_for("x"))
