ddb1f5550c9b5358e3029e75bc49f0324d212015
jw <choi.jae100@gmail.com>
2025-09-02 16:56:00 +0900

    add board constructor and init method

--- iuttt/board.py
+++ iuttt/board.py
@@ -1,6 +1,16 @@
 class Board:
-    def __init__(self) -> None:
-        pass
-        
+    def __init__(self, dimension: int = 3, rows: int | None = None, columns: int | None = None) -> None:
+        self._dimension = dimension
+        self._rows = rows 
+        self._columns = columns
+
+        self._init_board()
+
+    def _init_board(self):
+        if self._dimension and (not self._rows or not self._columns):
+            return list("." * self._dimension)
+
+        elif not self._dimension and (self._rows and self._columns):
+            return list("." * (self._rows * self._columns))
 
 
