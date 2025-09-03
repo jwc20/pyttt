e5bed2cb8a89fd881d8fec898fbdeea92f9784b2
jw <choi.jae100@gmail.com>
2025-09-03 15:32:41 +0900

    rename Position class to Game

--- iuttt/game.py
+++ iuttt/game.py
@@ -1,5 +1,69 @@
-from . import Position
+from copy import deepcopy
+
+DIM = 3
+SIZE = DIM * DIM
+
 
 class Game:
-    def __init__(self):
-        self.position = Position()
+    def __init__(self, board: str ="." * SIZE, turn: str ="x") -> None:
+        self.board = list(board)
+        self.turn = turn
+
+    def __repr__(self):
+        return "(%s, %s)" % (repr("".join(self.board)), repr(self.turn))
+
+    def __eq__(self, other):
+        return self.board == other.board and self.turn == other.turn
+
+    def choose(self, x, o):
+        return x if self.turn == "x" else o
+
+    def move(self, index):
+        self.board[index] = self.turn
+        self.turn = self.choose("o", "x")
+        return self
+
+    def possible_moves(self):
+        return [index for index, piece in enumerate(self.board) if piece == "."]
+
+    def is_win_for(self, piece):
+        is_match = lambda line: line.count(piece) == DIM
+        rows = [is_match(self.board[i : i + DIM]) for i in range(0, SIZE, DIM)]
+        cols = [is_match(self.board[i:SIZE:DIM]) for i in range(0, DIM)]
+        maj_diag = is_match(self.board[0 : SIZE : DIM + 1])
+        min_diag = is_match(self.board[DIM - 1 : SIZE - 1 : DIM - 1])
+        return any(rows) or any(cols) or maj_diag or min_diag
+
+    cache = {}
+    def minimax(self):
+        key = repr(self)
+        value = self.cache.get(key)
+        if value is not None:
+            # if the value is 0, then the value is considered False. so we need to check if it's None.
+            return value
+        if self.is_win_for("x"):
+            return self.board.count(".")
+        if self.is_win_for("o"):
+            return -self.board.count(".")
+        if self.board.count(".") == 0:
+            return 0
+
+        # TODO: find a way not to use deepcopy()
+        values = [
+            deepcopy(self).move(index).minimax() for index in self.possible_moves()
+        ]
+        value = self.choose(max, min)(values)
+        self.cache[key] = value
+        return value
+
+    def best_move(self):
+        fn = self.choose(max, min)
+        return fn(
+            self.possible_moves(),
+            key=lambda index: deepcopy(self).move(index).minimax(),
+        )
+
+    def is_game_end(self):
+        return (
+            self.is_win_for("x") or self.is_win_for("o") or self.board.count(".") == 0
+        )
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,11 +1,82 @@
+from iuttt import Game
 from unittest import TestCase
-from iuttt import Game, Position
 
 
 class TestGame(TestCase):
-    def set_up(self):
-        self.game = Game()
-
     def test_init(self):
-        self.set_up()
-        self.assertEqual(self.game.position, Position())
+        game = Game()
+        self.assertEqual(game.board, list("." * 9))
+        self.assertEqual(game.turn, "x")
+
+    def test_init_with_args(self):
+        game = Game("x........", "o")
+        self.assertEqual(game.board, list("x........"))
+        self.assertEqual(game.turn, "o")
+
+    def test_eq(self):
+        self.assertEqual(Game(), Game())
+
+    def test_move(self):
+        self.assertEqual(Game().move(1), Game(".x.......", "o"))
+
+    def test_possible_moves(self):
+        self.assertEqual(Game().move(1).possible_moves(), [0, 2, 3, 4, 5, 6, 7, 8])
+
+
+class TestIsWinFor(TestCase):
+    def test_no_win(self):
+        self.assertFalse(Game().is_win_for("x"))
+
+    def test_row(self):
+        self.assertTrue(Game("xxx......").is_win_for("x"))
+
+    def test_col(self):
+        self.assertTrue(Game("o..o..o..").is_win_for("o"))
+
+    def test_major_diagonal(self):
+        self.assertTrue(Game("x...x...x").is_win_for("x"))
+
+    def test_minor_diagonal(self):
+        self.assertTrue(Game("..x.x.x..").is_win_for("x"))
+
+
+class TestMinimax(TestCase):
+    def test_x_wins(self):
+        self.assertEqual(Game("xxx......").minimax(), 6)
+
+    def test_o_wins(self):
+        self.assertEqual(Game("ooo......").minimax(), -6)
+
+    def test_draw(self):
+        self.assertEqual(Game("xoxxoxoxo").minimax(), 0)
+
+    def test_x_wins_in_one(self):
+        self.assertEqual(Game("xx.......").minimax(), 6)
+
+    def test_o_wins_in_one(self):
+        self.assertEqual(Game("oo.......", "o").minimax(), -6)
+
+    def test_cache(self):
+        self.assertEqual(Game().minimax(), 0)
+
+
+class TestBestMove(TestCase):
+    def test_x(self):
+        self.assertEqual(Game("xx.......", "x").best_move(), 2)
+
+    def test_o(self):
+        self.assertEqual(Game("oo.......", "o").best_move(), 2)
+
+
+class TestIsGameEnd(TestCase):
+    def test_not_end(self):
+        self.assertFalse(Game().is_game_end())
+
+    def test_x_wins(self):
+        self.assertTrue(Game("xxx......").is_game_end())
+
+    def test_o_wins(self):
+        self.assertTrue(Game("ooo......").is_game_end())
+
+    def test_draw(self):
+        self.assertTrue(Game("xoxxoxoxo").is_game_end())
