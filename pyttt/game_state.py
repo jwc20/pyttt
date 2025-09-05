2fff46ab865fde3b7b7f62df68d67a0395e1181e
jw <choi.jae100@gmail.com>
2025-09-05 15:09:37 +0900

    change state names

--- pyttt/game_state.py
+++ pyttt/game_state.py
@@ -1,16 +1,33 @@
 from typing import Protocol
+from dataclasses import dataclass
 
 
 class GameState(Protocol):
     """
-    there are 3 states:
+    there are 4 states:
+    - setup
     - ready
     - playing
-    - locked (ended or before ready)
-    
+    - ended
     """
     pass
 
 
 class GameContext(Protocol):
+    def set_state(self, state: GameState): ...
+
+@dataclass
+class SetupGameState:
+    pass
+
+@dataclass
+class ReadyGameState:
+    pass
+
+@dataclass
+class PlayingGameState:
+    pass
+
+@dataclass
+class EndedGameState:
     pass
