3da9f429667926f53d0ab26fc1c244f87cff9046
jw <choi.jae100@gmail.com>
2025-09-05 17:30:00 +0900

    remove from init

--- pyttt/__init__.py
+++ pyttt/__init__.py
@@ -1,6 +1,5 @@
 from .game import Game
 from .board import Board
-from .board_state import BoardState, NormalState
 from .player import Player
 
-__all__ = ["Game", "Board", "BoardState", "NormalState", "Player"]
+__all__ = ["Game", "Board", "Player"]
