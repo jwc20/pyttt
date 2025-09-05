673545c895b4c10dd57e00723c66965389360f85
jw <choi.jae100@gmail.com>
2025-09-05 17:23:36 +0900

    clean up

--- pyttt/board.py
+++ pyttt/board.py
@@ -57,11 +57,6 @@ class Board:
             rows: int | None = None,
             columns: int | None = None
     ) -> None:
-        # self._variant = variant
-        # self._dimension = dimension
-        # self._rows = rows
-        # self._columns = columns
-
         self._config = {
             "variant": variant,
             "dimension": dimension,
@@ -85,39 +80,24 @@ class Board:
             - dimension (width of matrix)
             - rows and columns
         """
-        # if self._variant and not (self._dimension or (self._rows and self._columns)):
-        #     return self._init_variant_board()
-        # 
-        # if self._dimension and not (self._variant or (self._rows and self._columns)):
-        #     return "." * self._dimension
-        # 
-        # if (self._rows and self._columns) and not (self._variant or self._dimension):
-        #     return "." * (self._rows * self._columns)
-        # 
-        # if not (self._variant or self._dimension or (self._rows and self._columns)):
-        #     raise ValueError("Invalid board initialization")
-
-        if self._config["variant"] and self._config["dimension"] is None:
+        if (self._config["variant"]
+                and (self._config["dimension"] is None
+                     and (self._config["rows"] is None and self._config["columns"] is None))):
             return VariantStrategy(self._config).create_board()
 
-        if self._config["dimension"] and (
-                self._config["variant"] is None and self._config["rows"] is None and self._config["columns"] is None):
+        if (self._config["dimension"]
+                and (self._config["variant"] is None
+                     and (self._config["rows"] is None and self._config["columns"] is None))):
             return DimensionStrategy(self._config).create_board()
 
-        if (self._config["rows"] and self._config["columns"]) and (
-                self._config["variant"] is None and self._config["dimension"] is None):
+        if ((self._config["rows"]
+             and self._config["columns"])
+                and (self._config["variant"] is None and self._config["dimension"] is None)):
             return RowsColumnsStrategy(self._config).create_board()
 
         if not (self._config["variant"] or self._config["dimension"] or (
                 self._config["rows"] and self._config["columns"])):
             return "." * 9
-
-        # raise ValueError("Invalid board initialization")
-
-    # def _init_variant_board(self) -> str:
-    #     if self._variant == "classic":
-    #         return "." * 9
-    #     raise ValueError(f"Unknown variant: {self._variant}")
 
     # TODO
     def set_state(self):
