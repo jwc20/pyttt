a58e34e75402458c92062bc2f39169f5ba397103
jw <choi.jae100@gmail.com>
2025-09-06 13:42:38 +0900

    move example implementation to example.py

--- example.py
+++ example.py
@@ -1,6 +1,27 @@
 from pyttt import Board
 
 if __name__ == "__main__":
-    board = Board()
-    
-    # game = Game(board, players=["x", "o"])
+    _board_str = Board(dimension=(3 ** 2))
+
+    _dim = _board_str.get_dimension()
+    _coords = _board_str.get_coordinates_str(_dim)
+    _rows, _cols = _coords, _coords
+    _squares = _board_str.cross(_rows, _cols)
+    _coords_3 = _board_str.get_three_by_three(_coords)
+    _rows_3, _cols_3 = _coords_3, _coords_3
+    _all_boxes = _board_str.get_all_boxes(_rows_3, _cols_3)
+    _all_units = _board_str.get_all_units(_rows, _cols, _all_boxes)
+    _units = _board_str.get_units(_squares, _all_units)
+    _peers = _board_str.get_peers(_squares, _units)
+
+    from pprintpp import pprint
+
+    pprint(_board_str)
+    # pprint(_squares)
+    # pprint("coords_3", _coords_3)
+    # pprint(_all_boxes)
+    # pprint(_all_units)
+    # pprint(_units)
+    # pprint(_peers)
+    # pprint(_units["0,0"])
+    # pprint(_peers["0,0"])
--- pyttt/board.py
+++ pyttt/board.py
@@ -4,30 +4,30 @@ from pyttt.board_strategy import DimensionStrategy, RowsColumnsStrategy, Variant
 class Board:
     """
     the board is represented as a string of "x", "o", and "." characters
-    
-    ex: 
+
+    ex:
         - 3x3 board: "xoxoxoxox"
         - 9x9 board: "xxxxxxxxx x..x..xo. x..x..xo. ooooooooo x..x..xo. x..x..xo. xoxoxoxox x..x..xo. x..x..xo."
             - no whitespace in real board string
-    
+
     the board can be initialized in three ways:
         - by variant (classic, ultimate, etc.)
-        - by dimension 
+        - by dimension
         - by rows and columns
     """
 
     def __init__(
-            self,
-            variant: str | None = None,
-            dimension: int | None = None,
-            rows: int | None = None,
-            columns: int | None = None
+        self,
+        variant: str | None = None,
+        dimension: int | None = None,
+        rows: int | None = None,
+        columns: int | None = None,
     ) -> None:
         self._config = {
             "variant": variant,
             "dimension": dimension,
             "rows": rows,
-            "columns": columns
+            "columns": columns,
         }
 
         self.next = None
@@ -41,28 +41,33 @@ class Board:
 
     def _init_board(self) -> str | None:
         """
-        create a board by: 
+        create a board by:
             - variant
             - dimension (width of matrix)
             - rows and columns
         """
-        if (self._config["variant"]
-                and (self._config["dimension"] is None
-                     and (self._config["rows"] is None and self._config["columns"] is None))):
+        if self._config["variant"] and (
+            self._config["dimension"] is None
+            and (self._config["rows"] is None and self._config["columns"] is None)
+        ):
             return VariantStrategy(self._config).create_board()
 
-        if (self._config["dimension"]
-                and (self._config["variant"] is None
-                     and (self._config["rows"] is None and self._config["columns"] is None))):
+        if self._config["dimension"] and (
+            self._config["variant"] is None
+            and (self._config["rows"] is None and self._config["columns"] is None)
+        ):
             return DimensionStrategy(self._config).create_board()
 
-        if ((self._config["rows"]
-             and self._config["columns"])
-                and (self._config["variant"] is None and self._config["dimension"] is None)):
+        if (self._config["rows"] and self._config["columns"]) and (
+            self._config["variant"] is None and self._config["dimension"] is None
+        ):
             return RowsColumnsStrategy(self._config).create_board()
 
-        if not (self._config["variant"] or self._config["dimension"] or (
-                self._config["rows"] and self._config["columns"])):
+        if not (
+            self._config["variant"]
+            or self._config["dimension"]
+            or (self._config["rows"] and self._config["columns"])
+        ):
             return "." * 9
 
     # TODO
@@ -91,50 +96,22 @@ class Board:
         """
         return tuple(a + "," + b for a in vector_a for b in vector_b)
 
+    def get_three_by_three(self, board_str) -> tuple:
+        return tuple([board_str[i : i + 3] for i in range(0, len(board_str), 3)])
 
-    def get_three_by_three(self, board_str):
-        return [board_str[i:i+3] for i in range(0, len(board_str), 3)]
-
-    def get_all_boxes(self, rows, cols):
+    def get_all_boxes(self, rows, cols) -> list:
+        print(rows, cols)
         return [self.cross(rs, cs) for rs in rows for cs in cols]
-    
-    def get_all_units(self, rows, cols, boxes):
-        return [self.cross(rows, c) for c in cols] + [self.cross(r, cols) for r in rows] + boxes
-    
-    def get_all_peers(self, units):
-        return {s: set().union(*units[s]) - {s} for s in _squares}
-    
-    def get_units(self, squares, all_units):
+
+    def get_all_units(self, rows, cols, boxes) -> list:
+        return (
+            [self.cross(rows, c) for c in cols]
+            + [self.cross(r, cols) for r in rows]
+            + boxes
+        )
+
+    def get_units(self, squares, all_units) -> dict:
         return {s: tuple(u for u in all_units if s in u) for s in squares}
-    
-    def get_peers(self, squares, units):
+
+    def get_peers(self, squares, units) -> dict:
         return {s: set().union(*units[s]) - {s} for s in squares}
-    
-    
-
-
-if __name__ == "__main__":
-    _board_str = Board(dimension=(3 ** 2))
-    
-    
-    _dim = _board_str.get_dimension()
-    _coords = _board_str.get_coordinates_str(_dim)
-    _rows, _cols = _coords, _coords
-    _squares = _board_str.cross(_rows, _cols)
-    _coords_3 = _board_str.get_three_by_three(_coords)
-    _all_boxes = _board_str.get_all_boxes(_rows, _cols)
-    _all_units = _board_str.get_all_units(_rows, _cols, _all_boxes)
-    _units = _board_str.get_units(_squares, _all_units)
-    _peers = _board_str.get_peers(_squares, _units)
-
-    from pprintpp import pprint
-
-    print(_board_str)
-    print(_squares)
-    print("coords_3", _coords_3)
-    pprint(_all_boxes)
-    pprint(_all_units)
-    pprint(_units)
-    pprint(_peers)
-    pprint(_units["0,0"])
-    pprint(_peers["0,0"])
