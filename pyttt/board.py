2c10b28639168f0c63759c70ff2de408b6f74343
jw <choi.jae100@gmail.com>
2025-09-13 11:31:43 +0900

    refactor board.py, remove duplicate codes and use functions from utils.py

--- pyttt/board.py
+++ pyttt/board.py
@@ -1,34 +1,43 @@
+"""
+pyttt/board.py
+"""
+
 from pyttt.board_strategy import DimensionStrategy, RowsColumnsStrategy, VariantStrategy
-from pyttt.utils import insert_char_every_n, get_dimension, cross, get_coordinates, convert_to_grid
+from pyttt.utils import (
+    insert_char_every_n, 
+    get_dimension, 
+    cross, 
+    get_coordinates, 
+    convert_to_grid,
+    get_three_by_three,
+    get_all_boxes,
+    display_board as display_board_util,
+    get_box_index_from_coordinate,
+    get_box_from_coordinate,
+    get_board_str_from_box,
+    get_square_value,
+    get_all_units,
+    get_units,
+    get_peers,
+)
 import math
+
 
 class ScoreBoard:
     def __init__(self):
         self.score_board_str = ""
-        # if self.score_board_str == "":
-        #     self.score_board_str = self._init_score_board()
-        
         self.history = []
 
-        self.dimension = get_dimension(self.score_board_str)
-        self.coords = get_coordinates(self.dimension)
-        self.squares = cross(vector_a=self.coords, vector_b=self.coords)
-        self.grid = convert_to_grid(self.score_board_str, self.squares)
-        
-        # self.squares = cross(self.score_board_str, )
-        if len(self.score_board_str) > 0:
-            self.dimension = get_dimension(self.score_board_str)
-            self.coords = get_coordinates(self.dimension)
-            self.squares = cross(vector_a=self.coords, vector_b=self.coords)
-            self.grid = convert_to_grid(self.score_board_str, self.squares)
-    
+        self.dimension = get_dimension(self.score_board_str) if self.score_board_str else 0
+        self.coords = get_coordinates(self.dimension) if self.dimension > 0 else []
+        self.squares = cross(vector_a=self.coords, vector_b=self.coords) if self.coords else tuple()
+        self.grid = convert_to_grid(self.score_board_str, self.squares) if self.squares else {}
 
-    
     def update_with_scoreboard_grid(self, grid):
         self.grid = grid 
         self.score_board_str = "".join(self.grid.values())
     
-    def update(self,  board_str, event: str | None = None):
+    def update(self, board_str, event: str | None = None):
         print(board_str)
         dimension = get_dimension(board_str)
         if dimension < 9:
@@ -36,7 +45,6 @@ class ScoreBoard:
         
         coords = get_coordinates(dimension)
         squares = cross(vector_a=coords, vector_b=coords)
-        
 
         squares_count = len(squares)
         squares_exponent = int(math.log(squares_count, 3))
@@ -50,24 +58,9 @@ class ScoreBoard:
         
         self.score_board_str = ";".join(segments)
         return self.score_board_str
-        
 
 
 class Board:
-    """
-    the board is represented as a string of "x", "o", and "." characters
-
-    ex:
-        - 3x3 board: "xoxoxoxox"
-        - 9x9 board: "xxxxxxxxx x..x..xo. x..x..xo. ooooooooo x..x..xo. x..x..xo. xoxoxoxox x..x..xo. x..x..xo."
-            - no whitespace in real board string
-
-    the board can be initialized in three ways:
-        - by variant (classic, ultimate, etc.)
-        - by dimension
-        - by rows and columns
-    """
-
     def __init__(
             self,
             variant: str | None = None,
@@ -89,32 +82,23 @@ class Board:
         self.history = []
 
         self.board_str = board_str
-        # self.board = None
         if board_str is None:
-            self.board_str = self._init_board()  # board string
-
-        # self._squares = [Square(x, y) for x in range(self.get_dimension()) for y in range(self.get_dimension())]
-        # print(self._squares)
-
-        # TODO: implement state
-        # self._state: BoardState = NormalState(self)
+            self.board_str = self._init_board()
 
         if dimension is None:
             dimension = self.get_dimension()
             
-        self.coords = self.get_coordinates(dimension)
-        self.squares = self.cross(vector_a=self.coords, vector_b=self.coords)
-        self.coords_3 = self.get_three_by_three(self.coords)
-        self.boxes = self.get_all_boxes(rows=self.coords_3, cols=self.coords_3)
+        self.coords = get_coordinates(dimension)
+        self.squares = cross(vector_a=self.coords, vector_b=self.coords)
+        self.coords_3 = get_three_by_three(self.coords)
+        self.boxes = get_all_boxes(rows=self.coords_3, cols=self.coords_3)
         
-        self.grid = self.convert_to_grid(self.board_str, self.squares)
+        self.grid = convert_to_grid(self.board_str, self.squares)
         
         self.score_board: ScoreBoard | None = None
         
         if self.score_board is None:
             self._init_score_board()
-
-
 
     def _init_score_board(self) -> str:
         self.score_board = ScoreBoard()
@@ -138,7 +122,6 @@ class Board:
 
         return ";".join(segments)
     
-    
     def __str__(self):
         return self.board_str
     
@@ -147,8 +130,6 @@ class Board:
         
         if len(score_board.score_board_str) == 0:
             self.score_board.update(board_str=self.board_str)
-        
-        
         
     def detach_score_board(self):
         self.score_board = None
@@ -162,13 +143,6 @@ class Board:
         return self._config
 
     def _init_board(self) -> str | None:
-        """
-        create a board by:
-            - variant
-            - dimension (width of matrix)
-            - rows and columns
-        """
-
         board_str = ""
 
         if self._config["variant"] and (
@@ -193,235 +167,54 @@ class Board:
                 or self._config["dimension"]
                 or (self._config["rows"] and self._config["columns"])
         ):
-            board_str = "." * 9  # classic ttt
+            board_str = "." * 9
 
         return board_str
 
-    # TODO
     def set_state(self):
         pass
 
-    # TODO
-    # def __repr__(self):
-    #     """
-    #     returns string like "xxxxxxxxxx..x..xo.x..x..xo.ooooooooox..x..xo.x..x..xo.xoxoxoxoxx..x..xo.x..x..xo."
-    #     """
-    #     return self.board_str
-
-    # TODO
     def __eq__(self, other):
         pass
 
     def get_dimension(self) -> int:
-        return int(len(self.board_str) ** (1 / 2))
-
-    def get_coordinates(self, dim: int) -> list:
-        # return "".join([str(i) for i in range(dim)])
-        return [str(i) for i in range(dim)]
-
-    def cross(self, vector_a, vector_b) -> tuple:
-        """
-        get 3x3 boards from board string
-        (does not work for non-square matrices) -> TODO
-        do cross product of rows and columns to get all possible 3x3 boards
-        """
-        
-        
-        return tuple(a + "," + b for a in vector_a for b in vector_b)
-
-    def get_three_by_three(self, coords: list) -> tuple:
-        """
-        get 3x3 boards from board string
-        :param board_str: 
-        :return: tuple
-        
-        :example:
-            - 9x9 board
-            - return = ('012', '345', '678')
-        """
-        coord_str = "".join(coords)
-        return tuple([coord_str[i: i + 3] for i in range(0, len(coord_str), 3)])
-
-    def get_all_boxes(self, rows, cols) -> list:
-        """
-        get all possible 3x3 boxes
-        :param rows: 
-        :param cols: 
-        :return: list of 3x3 boxes
-        
-        :example:
-            - rows = ('012', '345', '678')
-            - cols = ('012', '345', '678')
-            - return = [
-                        ('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2'),
-                        ('0,3', '0,4', '0,5', '1,3', '1,4', '1,5', '2,3', '2,4', '2,5'),
-                        ('0,6', '0,7', '0,8', '1,6', '1,7', '1,8', '2,6', '2,7', '2,8'),
-                        ('3,0', '3,1', '3,2', '4,0', '4,1', '4,2', '5,0', '5,1', '5,2'),
-                        ('3,3', '3,4', '3,5', '4,3', '4,4', '4,5', '5,3', '5,4', '5,5'),
-                        ('3,6', '3,7', '3,8', '4,6', '4,7', '4,8', '5,6', '5,7', '5,8'),
-                        ('6,0', '6,1', '6,2', '7,0', '7,1', '7,2', '8,0', '8,1', '8,2'),
-                        ('6,3', '6,4', '6,5', '7,3', '7,4', '7,5', '8,3', '8,4', '8,5'),
-                        ('6,6', '6,7', '6,8', '7,6', '7,7', '7,8', '8,6', '8,7', '8,8'),
-            ]
-        """
-        return [self.cross(rs, cs) for rs in rows for cs in cols]
+        return get_dimension(self.board_str)
 
     def display_board(self, t: str | None = None):
-        """
-        convert board string to grid
-        """
-        if t is None:
-            t = "value"
-        rows, cols = self.coords, self.coords
-        if self.grid is None:
-            return "None"
-        result_row = []
-        result = []
-        for c in cols:
-            result_row.append("\n")
-            for r in rows:
-                if t == "coordinate":
-                    result_row.append(str(r + "," + c).center(10))
-                else:
-                    result_row.append(self.grid[r + "," + c])
-        result.append(" ".join(result_row))
-        return "".join(result)
-
-    # def display_board(self):
-    #     """
-    #     convert board string to grid
-    # 
-    #     TODO:
-    #     (works only for 3x3 and 9x9 boards)
-    #     """
-    #     
-    #     rows, cols = self.coords, self.coords
-    #     
-    #     if self.grid is None:
-    #         return "None"
-    # 
-    #     width = 1 + max(len(self.grid[s]) for s in self.grid)
-    #     line = "+".join(["-" * (width * 3)] * (len(cols) // 3))
-    # 
-    #     result = []
-    #     for r in rows:
-    #         row_str = "".join(
-    #             self.grid[r + "," + c].center(width)
-    #             + ("|" if (int(c) + 1) % 3 == 0 and int(c) + 1 < len(cols) else "")
-    #             for c in cols
-    #         )
-    #         result.append(row_str)
-    #         if (int(r) + 1) % 3 == 0 and int(r) + 1 < len(rows):
-    #             result.append(line)
-    # 
-    #     return "\n".join(result)
+        return display_board_util(self.coords, self.grid, t)
 
     def get_box_index_from_coordinate(self, xy: str) -> int | None:
-        """
-        get box number from coordinate
-        :param xy: 
-        :return: int
-        
-        :example:
-            - xy = "0,0"
-            - return = 0
-        """
-        for box in self.boxes:
-            if xy in box:
-                return self.boxes.index(box)
+        return get_box_index_from_coordinate(xy, self.boxes)
 
     def get_box_from_coordinate(self, xy: str) -> tuple:
-        """
-        get box from coordinate
-        :param xy: 
-        :return: tuple
-        
-        :example:
-            - xy = "0,0"
-            - return = ('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2')
-        """
-        return self.boxes[self.get_box_index_from_coordinate(xy)]
+        return get_box_from_coordinate(xy, self.boxes)
 
     def get_board_str_from_box(self, box, grid) -> str:
-        """
-        get board string from box
-        :param box: 
-        :param grid: 
-        :return: string
-        
-        :example:
-            - box = ('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2')
-            - grid = {'0,0': '.', '0,1': '.', '0,2': '.', '0,3': '.', '0,4': '.', '0,5': '.', '0,6': '.', '0,7': '.', '0,8': '.', '1,0': '.', '1,1': '.', '1,2': '.', '1,3': '.', '1,4': '.', '1,5': '.', '1,6': '.', '1,7': '.', '1,8': '.', '2,0': '.', '2,1': '.', '2,2': '.', '2,3': '.', '2,4': '.', '2,5': '.', '2,6': '.', '2,7': '.', '2,8': '.', '3,0': '.', '3,1': '.', '3,2': '.', '3,3': '.', '3,4': '.', '3,5': '.', '3,6': '.', '3,7': '.', '3,8': '.', '4,0': '.', '4,1': '.', '4,2': '.', '4,3': '.', '4,4': '.', '4,5': '.', '4,6': '.', '4,7': '.', '4,8': '.', '5,0': '.', '5,1': '.', '5,2': '.', '5,3': '.', '5,4': '.', '5,5': '.', '5,6': '.', '5,7': '.', '5,8': '.', '6,0': '.', '6,1': '.', '6,2': '.', '6,3': '.', '6,4': '.', '6,5': '.', '6,6': '.', '6,7': '.', '6,8': '.', '7,0': '.', '7,1': '.', '7,2': '.', '7,3': '.', '7,4': '.', '7,5': '.', '7,6': '.', '7,7': '.', '7,8': '.', '8,0': '.', '8,1': '.', '8,2': '.', '8,3': '.', '8,4': '.', '8,5': '.', '8,6': '.', '8,7': '.', '8,8': '.'}
-            - return = "........."
-        """
-        result = []
-        for xy in box:
-            # xy is str coordinates like "0,0"
-            square_value = self.get_square_value(xy, grid)
-            result.append(square_value)
-        return "".join(result)
+        return get_board_str_from_box(box, grid)
 
     def get_square_value(self, xy, grid):
-        """get square value from coordinates"""
-        return grid[xy]
+        return get_square_value(xy, grid)
 
     def to_list(self):
         return list(self.board_str)
 
-    ##################################################################
-    # will probably not use
-    ##################################################################
-
     def get_all_units(self, rows, cols, boxes) -> list:
-        return (
-                [self.cross(rows, c) for c in cols]
-                + [self.cross(r, cols) for r in rows]
-                + boxes
-        )
+        return get_all_units(rows, cols, boxes)
 
     def get_units(self, squares, all_units) -> dict:
-        """
-        get all units that contain each square
-
-        a unit is a row, column, or box; each unit is a tuple of 9(or more) squares
-        """
-        return {s: tuple(u for u in all_units if s in u) for s in squares}
+        return get_units(squares, all_units)
 
     def get_peers(self, squares, units) -> dict:
-        """the squares that share a unit are called peers."""
-        return {s: set().union(*units[s]) - {s} for s in squares}
+        return get_peers(squares, units)
 
-    def convert_to_grid(self, board_str, squares):
-        """Convert a string to a Tic-Tac-Toe grid."""
-        import re
-
-        vals = re.findall(r"[.XOxo]", board_str)
-        return {
-            s: v.lower() if v.lower() in "xo" else "." for s, v in zip(squares, vals)
-        }
-
-    ##################################################################
-
-    # TODO
     def place_mark(self, mark, xy):
         self.grid[xy] = mark
         self.board_str = "".join(self.grid.values())
-        # print(self.board_str)
-        # self.grid = self.convert_to_grid(self.board_str, self.squares)
-        # print(self.grid)
-        
-        
 
     def get_legal_move(self):
         pass
 
     def get_allowed_boxes(self):
-        """for ultimate tic-tac-toe"""
         if self.config is None or self.config["variant"] != "ultimate" or len(self.board_str) != 81:
             return ""
-
-        # allowed_boxes = ""
         return
-
-
-
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,6 +1,20 @@
+"""
+tests/test_board.py
+"""
+
 from unittest import TestCase
 
 from pyttt.board import Board
+from pyttt.utils import (
+    convert_to_grid, 
+    cross,
+    get_coordinates,
+    get_three_by_three,
+    get_all_boxes,
+    get_all_units,
+    get_units,
+    get_peers
+)
 
 
 class TestBoard(TestCase):
@@ -35,45 +49,45 @@ class TestBoard(TestCase):
     def test_cross(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
-        self.assertEqual(board.cross(coords, coords), ("0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"))
+        coords = get_coordinates(dim)
+        self.assertEqual(cross(coords, coords), ("0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2"))
     
     def test_get_three_by_three(self):
         board = Board(dimension=9)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
-        self.assertEqual(board.get_three_by_three(coords), ("012", "345", "678"))
+        coords = get_coordinates(dim)
+        self.assertEqual(get_three_by_three(coords), ("012", "345", "678"))
 
     def test_get_all_boxes(self):
         board = Board(dimension=9)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
-        coords_3 = board.get_three_by_three(coords)
+        coords = get_coordinates(dim)
+        coords_3 = get_three_by_three(coords)
         rows, cols = coords_3, coords_3
         output = "[('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2'),('0,3', '0,4', '0,5', '1,3', '1,4', '1,5', '2,3', '2,4', '2,5'),('0,6', '0,7', '0,8', '1,6', '1,7', '1,8', '2,6', '2,7', '2,8'),('3,0', '3,1', '3,2', '4,0', '4,1', '4,2', '5,0', '5,1', '5,2'),('3,3', '3,4', '3,5', '4,3', '4,4', '4,5', '5,3', '5,4', '5,5'),('3,6', '3,7', '3,8', '4,6', '4,7', '4,8', '5,6', '5,7', '5,8'),('6,0', '6,1', '6,2', '7,0', '7,1', '7,2', '8,0', '8,1', '8,2'),('6,3', '6,4', '6,5', '7,3', '7,4', '7,5', '8,3', '8,4', '8,5'),('6,6', '6,7', '6,8', '7,6', '7,7', '7,8', '8,6', '8,7', '8,8')]"
-        self.assertEqual(board.get_all_boxes(rows, cols), eval(output))
+        self.assertEqual(get_all_boxes(rows, cols), eval(output))
 
     def test_get_all_units(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
-        coords_3 = board.get_three_by_three(coords)
+        coords = get_coordinates(dim)
+        coords_3 = get_three_by_three(coords)
         rows, cols = coords, coords
         rows_3, cols_3 = coords_3, coords_3
-        boxes = board.get_all_boxes(rows_3, cols_3)
+        boxes = get_all_boxes(rows_3, cols_3)
         output = "[('0,0', '1,0', '2,0'),('0,1', '1,1', '2,1'),('0,2', '1,2', '2,2'),('0,0', '0,1', '0,2'),('1,0', '1,1', '1,2'),('2,0', '2,1', '2,2'),('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2')]"
         self.assertEqual(board.get_all_units(rows, cols, boxes), eval(output))
 
     def test_get_units(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
+        coords = get_coordinates(dim)
         rows, cols = coords, coords
-        squares = board.cross(rows, cols)
-        coords_3 = board.get_three_by_three(coords)
+        squares = cross(rows, cols)
+        coords_3 = get_three_by_three(coords)
         rows_3, cols_3 = coords_3, coords_3
-        all_boxes = board.get_all_boxes(rows_3, cols_3)
-        all_units = board.get_all_units(rows, cols, all_boxes)
+        all_boxes = get_all_boxes(rows_3, cols_3)
+        all_units = get_all_units(rows, cols, all_boxes)
         units = board.get_units(squares, all_units)
         output = "(('0,0', '1,0', '2,0'),('0,0', '0,1', '0,2'),('0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2'))"
         self.assertEqual(units["0,0"], eval(output))
@@ -81,14 +95,14 @@ class TestBoard(TestCase):
     def test_get_peers(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
+        coords = get_coordinates(dim)
         rows, cols = coords, coords
-        squares = board.cross(rows, cols)
-        coords_3 = board.get_three_by_three(coords)
+        squares = cross(rows, cols)
+        coords_3 = get_three_by_three(coords)
         rows_3, cols_3 = coords_3, coords_3
-        all_boxes = board.get_all_boxes(rows_3, cols_3)
-        all_units = board.get_all_units(rows, cols, all_boxes)
-        units = board.get_units(squares, all_units)
+        all_boxes = get_all_boxes(rows_3, cols_3)
+        all_units = get_all_units(rows, cols, all_boxes)
+        units = get_units(squares, all_units)
         peers = board.get_peers(squares, units)
         output = "{'2,2', '1,0', '0,2', '0,1', '1,2', '1,1', '2,0', '2,1'}"
         self.assertEqual(peers["0,0"], eval(output))
@@ -97,31 +111,25 @@ class TestBoard(TestCase):
     def test_get_board_str_from_box(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
+        coords = get_coordinates(dim)
         rows, cols = coords, coords
-        squares = board.cross(rows, cols)
-        coords_3 = board.get_three_by_three(coords)
+        squares = cross(rows, cols)
+        coords_3 = get_three_by_three(coords)
         rows_3, cols_3 = coords_3, coords_3
-        boxes = board.get_all_boxes(rows_3, cols_3)
+        boxes = get_all_boxes(rows_3, cols_3)
         box_0 = boxes[0]
-        grid = board.convert_to_grid(board.board_str, squares)
+        grid = convert_to_grid(board.board_str, squares)
         output = "........."
         self.assertEqual(board.get_board_str_from_box(box_0, grid), output)
 
 
     def test_get_square_value(self):
-        # TODO: test square values that is not "." (e.g. "x", "o")
         board = Board(dimension=9)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
+        coords = get_coordinates(dim)
         rows, cols = coords, coords
-        squares = board.cross(rows, cols)
-        # coords_3 = board.get_three_by_three(coords)
-        # rows_3, cols_3 = coords_3, coords_3
-        # boxes = board.get_all_boxes(rows_3, cols_3)
-        # box_0 = boxes[0]
-        grid = board.convert_to_grid(board.board_str, squares)
-        # board_0_str = board.get_board_str_from_box(box_0, grid)
+        squares = cross(rows, cols)
+        grid = convert_to_grid(board.board_str, squares)
         square_01 = board.get_square_value("0,1", grid)
         self.assertEqual(square_01, ".")
 
@@ -129,16 +137,15 @@ class TestBoard(TestCase):
     def test_parse(self):
         board = Board(dimension=3)
         dim = board.get_dimension()
-        coords = board.get_coordinates(dim)
+        coords = get_coordinates(dim)
         rows, cols = coords, coords
-        squares = board.cross(rows, cols)
+        squares = cross(rows, cols)
 
-        grid = board.convert_to_grid(board.board_str, squares)
+        grid = convert_to_grid(board.board_str, squares)
         output={'0,0': '.', '0,1': '.', '0,2': '.', '1,0': '.', '1,1': '.', '1,2': '.', '2,0': '.', '2,1': '.', '2,2': '.'}
         self.assertEqual(grid, output)
 
 
-    # TODO: test for larger boxes
     def test_get_box_number_from_coordinate(self):
         board = Board(dimension=9)
     
